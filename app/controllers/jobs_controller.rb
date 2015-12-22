6ca249553804fcee7011012257f613389ffb36ef
tkhuynh <thequest2311@yahoo.com>
2015-12-22 00:51:35 -0800

    add relationship between job and user, only logged user can post jobs, edit and delete his own job post

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,32 +18,55 @@ class JobsController < ApplicationController
   end
 
   def new
-  	@job = Job.new
+    #don't let non logged in user see new job form
+    if current_user
+      @job = Job.new
+    else
+      redirect_to login_path
+    end
   end
 
   def create
-  	@job = Job.new(job_params)
-  	if @job.save
-  		redirect_to job_path(@job)
-  	else
-  		redirect_to new_job_path
-  	end
+    #prevent non logged in user to create new job
+    if current_user
+    	@job = current_user.jobs.new(job_params)
+    	if @job.save
+    		redirect_to job_path(@job)
+    	else
+    		redirect_to new_job_path
+    	end
+    else
+      redirect_to login_path
+    end
   end
 
   def edit
+    #prevent non logged in user to see edit job form
+    unless current_user == @job.user
+      redirect_to job_path(@job)
+    end
   end
 
   def update
-  	if @job.update_attributes(job_params)
-  		redirect_to job_path(@job)
-  	else
-  		redirect_to edit_job_path
-  	end
+    #prevent non logged in user to edit job
+    if current_user == @job.user
+    	if @job.update_attributes(job_params)
+    		redirect_to job_path(@job)
+    	else
+    		redirect_to edit_job_path
+    	end
+    else
+      redirect_to job_path(@job)
+    end
   end
 
   def destroy
-  	@job.destroy
-  	redirect_to jobs_path
+    if current_user == @job.user
+    	@job.destroy
+    	redirect_to jobs_path
+    else
+      redirect_to job_path(@job)
+    end
   end
 
 private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :jobs
 
 	# validates :first_name, presence: true
 	# validates :last_name, presence: true
